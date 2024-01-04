ca71572f5fe823cdc32193a03db36e693ff96711
IamAmit <amitkumarsatapathy645@gmail.com>
2024-01-04 17:51:03 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Board from "@/components/Board"
+import Menu from "@/components/Menu"
+import Toolbox from "@/components/Toolbox"
+export default function Home() {
+  return (
+    <>
+    <Menu/>
+    <Toolbox/>
+    <Board/>
+    </>
+  )
+}
